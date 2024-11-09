567c9e845ed629913d73c4a7d807a356f64ea00f
Ken Yeh <yehken@gmail.com>
2024-11-08 23:09:51 -0800

    update images path

--- fashion-blog-lab/src/components/Article.jsx
+++ fashion-blog-lab/src/components/Article.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+// Images
+import image1 from '../img/blog-image-1.jpg';
+import image2 from '../img/blog-image-2.jpg';
+
 function Article({ date, title, imageSrc, description }) {
+  const images = {
+    'blog-image-1.jpg': image1,
+    'blog-image-2.jpg': image2,
+  };
+
   return (
     <article>
       <h3>{date}</h3>
       <h4>{title}</h4>
-      <img src={imageSrc} alt={title} />
+      <img src={images[imageSrc]} alt={title} />
       <p>{description}</p>
       <a href="#" className="continue-link">Continues...</a>
     </article>
